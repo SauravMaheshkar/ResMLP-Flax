aa1813152e7b4caa10561e210003277c37cccd03
SauravMaheshkar <61241031+SauravMaheshkar@users.noreply.github.com>
2021-07-13 12:18:59 +0530

    Remove einops layers

--- resmlp_flax/utils.py
+++ resmlp_flax/utils.py
@@ -1,14 +1,12 @@
-import functools
 from typing import Sequence
 
 import jax.numpy as jnp
 import numpy as np
 from chex import Array
-from einops.einops import EinopsError, TransformRecipe, _prepare_transformation_recipe
 from flax import linen as nn
 from jax import lax
 
-__all__ = ["gelu", "Sequential", "Reduce"]
+__all__ = ["gelu", "Sequential"]
 
 
 class gelu(nn.Module):
@@ -38,47 +36,3 @@ class Sequential(nn.Module):
         for layer in self.layers:
             x = layer(x)
         return x
-
-
-class ReduceMixin:
-    """
-    From einops/einops/layers/__init__.py
-    """
-
-    def __init__(self, pattern, reduction, **axes_lengths):
-        super().__init__()
-        self.pattern = pattern
-        self.reduction = reduction
-        self.axes_lengths = axes_lengths
-        self._recipe = self.recipe()  # checking parameters
-
-    def __repr__(self):
-        params = "{!r}, {!r}".format(self.pattern, self.reduction)
-        for axis, length in self.axes_lengths.items():
-            params += ", {}={}".format(axis, length)
-        return "{}({})".format(self.__class__.__name__, params)
-
-    @functools.lru_cache(maxsize=1024)
-    def recipe(self) -> TransformRecipe:
-        try:
-            hashable_lengths = tuple(sorted(self.axes_lengths.items()))
-            return _prepare_transformation_recipe(
-                self.pattern, operation=self.reduction, axes_lengths=hashable_lengths
-            )
-        except EinopsError as e:
-            raise EinopsError(" Error while preparing {!r}\n {}".format(self, e))
-
-    def _apply_recipe(self, x):
-        return self._recipe.apply(x)
-
-
-class Reduce(nn.Module):
-    pattern: str
-    reduction: str
-
-    def setup(self):
-        self.reducer = ReduceMixin(self.pattern, self.reduction)
-
-    @nn.compact
-    def __call__(self, input):
-        return self.reducer._apply_recipe(input)
